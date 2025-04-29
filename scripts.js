1e5ce47db32eef75a5a74b8c589f1c4378eddde7
Najmo Aden <najmoaden138@gmail.com>
2025-04-29 11:15:48 +0300

    :wrench: refactor: work with classes

--- scripts.js
+++ scripts.js
@@ -5,7 +5,25 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
 
+//Opening the modal
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//Closing the modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//Event listener for opening the modal
 for (let i = 0; i < btnOpenModal.length; i++)
-  console.log(btnOpenModal[i].textContent);
+  btnOpenModal[i].addEventListener('click', openModal);
+
+//Event listener for closing the modal
+btnCloseModal.addEventListener('click', closeModal);
+
+//Event listener for closing the modal when clicking outside of it
+overlay.addEventListener('click', closeModal);
